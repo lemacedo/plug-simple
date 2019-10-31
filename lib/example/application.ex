3eb4138d35699e997387404d36c1b08ae1a7a40f
Leandro Macedo <l.macedo@manusis4.com>
2019-10-31 01:28:36 -0300

    simples example using elixir plug

---
+++ lib/example/application.ex
@@ -0,0 +1,17 @@
+defmodule Example.Application do
+  use Application
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: Example.Router, options: [port: 8080]}
+    ]
+    opts = [strategy: :one_for_one, name: Example.Supervisor]
+
+    Logger.info("Starting application...")
+
+    Supervisor.start_link(children, opts)
+  end
+
+  defp cowboy_port, do: Application.get_env(:example, :cowboy_port, 8080)
+end
