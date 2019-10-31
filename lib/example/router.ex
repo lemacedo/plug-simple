2a0f3a3f420477092abfb417d18aa17572bb8870
Leandro Macedo <l.macedo@manusis4.com>
2019-10-31 03:40:33 -0300

    Add httpoison

--- lib/example/router.ex
+++ lib/example/router.ex
@@ -7,6 +7,11 @@ defmodule Example.Router do
   plug Plug.Parsers, parsers: [:urlencoded, :multipart]
   plug VerifyRequest, fields: ["content", "mimetype"], paths: ["/upload"]
   plug :match
+  plug Plug.Parsers,
+    parsers: [:json],
+    pass:  ["application/json"],
+   json_decoder: Jason
+    
   plug :dispatch
 
   get "/" do
@@ -15,6 +20,56 @@ defmodule Example.Router do
 
   get "/upload" do
     send_resp(conn, 201, "Uploaded")
+  end
+
+  post "/hello" do
+    IO.inspect conn.body_params # Prints JSON POST body
+    send_resp(conn, 200, "Success!")
+  end
+
+  post "/bla" do
+    HTTPoison.post('https://delivery-center-recruitment-ap.herokuapp.com/','{
+        "externalCode": "9987071",
+        "storeId": 282,
+        "subTotal": "49.90",
+        "deliveryFee": "5.14",
+        "total": "55.04",
+        "country": "BR",
+        "state": "SP",
+        "city": "Cidade de Testes",
+        "district": "Bairro Fake",
+        "street": "Rua de Testes Fake",
+        "complement": "galpao",
+        "latitude": -23.629037,
+        "longitude":  -46.712689,
+        "dtOrderCreate": "2019-06-27T19:59:08.251Z",
+        "postalCode": "85045020",
+        "number": "0",
+        "total_shipping": 5.14,
+        "items": [
+            {
+                "externalCode": "IT4801901403",
+                "name": "Produto de Testes",
+                "price": 49.9,
+                "quantity": 1,
+                "total": 49.9,
+                "subItems": []
+            }
+        ],
+        "payments": [
+            {
+                "type": "CREDIT_CARD",
+                "value": 55.04
+            }
+        ],
+        "customer": {
+        	"externalCode": "136226073",
+            "name": "JOHN DOE",
+            "email": "john@doe.com",
+            "contact": "41999999999"
+        }
+    }')
+
   end
 
   match _ do
